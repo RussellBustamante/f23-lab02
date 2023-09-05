fa4b1bb080e61e6240f0a3735d8e99f6d97b8ad8
Anthony Bustamante <anthony@anthonys-mbp-7.wifi.local.cmu.edu>
2023-09-05 15:12:48 -0400

    Generalized the declaration in main; Documented Shape.java

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
@@ -1,5 +1,10 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public interface Shape {
+    /** 
+     * Returns the area of the shape.
+     * 
+     * @return the area of the shape
+     */
     public double getArea();
 }
